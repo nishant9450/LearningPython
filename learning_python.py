51318aaf3497070b52987bd11f8721efadbd6a2b
nishant9450 <nishant9450@gmail.com>
2024-11-17 11:51:49 +0530

    started adding while loop

--- learning_python.py
+++ learning_python.py
@@ -10,3 +10,26 @@ elif a == b:
 else:
     print("b is greater a")
 
+# while loop
+i = 1
+while i <6:
+    print(i)
+    i += 1
+
+# With break statement we can stop the loop even if the condition is still true
+i = 5
+while i <15:
+    print(i)
+    if i ==10:
+        break
+    i +=1
+
+# With the continue statement we can stop the current iteration, and continue with the next
+# Note that number 103 is missing in the result
+m = 100
+while m <105:
+    m +=1
+    if m ==103:
+        continue
+    print(m)
+
