5fd98f5d8f5328fab121019d418136f6d8d8efaa
nishant9450 <nishant9450@gmail.com>
2024-11-17 11:38:43 +0530

    started adding python learning command

--- learning_python.py
+++ learning_python.py
@@ -1,1 +1,12 @@
 print("Nishant is greatest Python developer")
+
+# if else
+a = 200
+b= 333
+if a >b:
+    print('a is greter then b')
+elif a == b:
+    print('print a equal b')
+else:
+    print("b is greater a")
+
