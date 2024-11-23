09722b7f20ec149916d3328d99fe8713c130074e
nishant9450 <nishant9450@gmail.com>
2024-11-23 19:57:41 +0530

    adding class and object

--- learning_python.py
+++ learning_python.py
@@ -309,6 +309,74 @@ print(cars)
 cars.remove('honda')
 print(cars)
 
+"""
+Python Classes/Objects
+Python is an object oriented programming language.
+Almost everything in Python is an object, with its properties and methods.
+A Class is like an object constructor, or a "blueprint" for creating objects.
+"""
+
+# To create a class, use the keyword class:
+# Create a class named MyClass, with a property named x:
+
+class MyClass:
+    x=5
+
+"""
+Create Object
+Now we can use the class named MyClass to create objects:
+Create an object named p1, and print the value of x:
+"""
+
+p1= MyClass
+print(p1.x)
+
+"""
+The __init__() Function
+The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+To understand the meaning of classes we have to understand the built-in __init__() function.
+All classes have a function called __init__(), which is always executed when the class is being initiated.
+Use the __init__() function to assign values to object properties, or other operations that are necessary to do 
+when the object is being created
+"""
+
+# Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+p2= Person('Nishant',39)
+
+print(p2.name)
+print(p2.age)
+
+# The string representation of an object WITHOUT the __str__() function:
+
+class Person:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+p2= Person('Nishant',39)
+print(p2)
+
+# The string representation of an object WITH the __str__() function:
+class Banda:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} ({self.age})"
+
+p3 = Banda('Deepa',21)
+print(p3)
+
+
+
+
 
 
 
